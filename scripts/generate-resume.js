3f7df987a1199971027ef919f99aa77b9461d2d2
Kirill Kashin <47711872+Icearcticfox@users.noreply.github.com>
2026-05-04 23:29:57 +0300

    Update repository files (exclude favicon)

--- scripts/generate-resume.js
+++ scripts/generate-resume.js
@@ -212,6 +212,7 @@ const html = `<!doctype html>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>${header.name} — Resume</title>
+    <link rel="icon" type="image/png" href="/favicon.png" />
     <link rel="preload" href="/fonts/cmu-serif-500-roman.woff2" as="font" type="font/woff2" crossorigin />
     <link rel="preload" href="/fonts/cmu-serif-700-roman.woff2" as="font" type="font/woff2" crossorigin />
     <link rel="stylesheet" href="./style.css" />
